238a6ad348ddf3743ec82486e20414287e9e770d
Barros99 <jeanmeneses@hotmail.com>
2022-07-04 12:34:51 -0300

    feat: DeliveryController

--- src/main/java/com/future8/droneapp/controller/DeliveryController.java
+++ src/main/java/com/future8/droneapp/controller/DeliveryController.java
@@ -1,5 +1,31 @@
 package com.future8.droneapp.controller;
 
+import com.future8.droneapp.model.Delivery;
+import com.future8.droneapp.service.DeliveryService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/delivery")
 public class DeliveryController {
-    
+
+  @Autowired private DeliveryService deliveryService;
+
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Delivery create(@RequestBody Delivery deliveryRequest) {
+    return deliveryService.create(deliveryRequest);
+  }
+
+  @GetMapping
+  public List<Delivery> getDeliveries() {
+    return deliveryService.getDeliveries();
+  }
 }
