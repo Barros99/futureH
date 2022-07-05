bbfc31e502ff280b448968724d2090dcc1373ff9
Barros99 <jeanmeneses@hotmail.com>
2022-07-05 10:37:14 -0300

    feat: enable swagger2

--- src/main/java/com/future8/droneapp/DroneDeliveryApplication.java
+++ src/main/java/com/future8/droneapp/DroneDeliveryApplication.java
@@ -3,7 +3,10 @@ package com.future8.droneapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class DroneDeliveryApplication {
 
   public static void main(String[] args) {
