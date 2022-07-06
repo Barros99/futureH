49a542b8df1ad8bd13f8e9b309d525aea629f03d
Barros99 <jeanmeneses@hotmail.com>
2022-07-06 18:12:57 -0300

    feat: update and delete

--- src/main/java/com/future8/droneapp/controller/DeliveryController.java
+++ src/main/java/com/future8/droneapp/controller/DeliveryController.java
@@ -5,9 +5,11 @@ import com.future8.droneapp.service.DeliveryService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,4 +32,19 @@ public class DeliveryController {
   public List<Delivery> getDeliveries() {
     return deliveryService.getDeliveries();
   }
+
+  /** Update a drone. */
+  @PutMapping("/{id}")
+  @ResponseStatus(code = HttpStatus.OK)
+  public Delivery update(@PathVariable(value = "id") Integer id, @RequestBody Delivery delivery) {
+    return deliveryService.update(id, delivery);
+  }
+
+  /** Delete a drone. */
+  @DeleteMapping("/{id}")
+  @ResponseStatus(code = HttpStatus.OK)
+  public void delete(@PathVariable(value = "id") Integer id) {
+    deliveryService.delete(id);
+  }
+
 }
