d5a670d4aa9d1bc96a1b9e0fa8594a4ae07d4cb9
Rick Sousa <rickssti@outlook.com>
2022-07-07 12:30:28 -0300

    refactor delivery and drone tests

---
+++ src/test/java/com/future8/droneapp/controlller/DeliveryTest.java
@@ -0,0 +1,112 @@
+package com.future8.droneapp.controlller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.future8.droneapp.model.Delivery;
+import com.future8.droneapp.model.Drone;
+import com.future8.droneapp.repository.DeliveryRepository;
+import com.future8.droneapp.repository.DroneRepository;
+import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DeliveryTest {
+  @Autowired private MockMvc mockMvc;
+
+  @SpyBean private DeliveryRepository deliveryRepository;
+  @SpyBean private DroneRepository droneRepository;
+
+  @Autowired private ObjectMapper objectMapper;
+
+  @BeforeEach
+  public void initEach() {
+    deliveryRepository.deleteAll();
+  }
+
+  @Test
+  @DisplayName("1 - Delivery can be created with sucess")
+  public void create() throws JsonProcessingException, Exception {
+    Delivery delivery = new Delivery();
+    delivery.setDeliveredDate(LocalDate.now());
+    delivery.setDestiny("destiny");
+    delivery.setStatus("status");
+
+    Drone drone = new Drone(15151, 46545);
+
+    droneRepository.save(drone);
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post("/delivery/{id}", drone.getId())
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(delivery)))
+        .andExpect(MockMvcResultMatchers.status().isCreated());
+  }
+
+  @Test
+  @DisplayName("2 - List all deliveries")
+  public void retornaTodosOsDrones() throws Exception {
+    Delivery delivery = new Delivery();
+    delivery.setDeliveredDate(LocalDate.now());
+    delivery.setDestiny("destiny");
+    delivery.setStatus("status");
+
+    Drone drone = new Drone(15151, 46545);
+
+    droneRepository.save(drone);
+    mockMvc
+        .perform(MockMvcRequestBuilders.get("/delivery").contentType("application/json"))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  @Test
+  @DisplayName("3 - Delivery can be edited with sucess")
+  public void atualizaStatusDrone() throws Exception {
+    Delivery delivery = new Delivery();
+    delivery.setDeliveredDate(LocalDate.now());
+    delivery.setDestiny("destiny");
+    delivery.setStatus("status");
+
+    Drone drone = new Drone(15151, 46545);
+
+    droneRepository.save(drone);
+    deliveryRepository.save(delivery);
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.put("/delivery/{id}", delivery.getId())
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(delivery)))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  @Test
+  @DisplayName("4 - Delivery can be deleted with sucess")
+  public void deletaDronePeloId() throws Exception {
+    Delivery delivery = new Delivery();
+    delivery.setDeliveredDate(LocalDate.now());
+    delivery.setDestiny("destiny");
+    delivery.setStatus("status");
+
+    Drone drone = new Drone(15151, 46545);
+
+    droneRepository.save(drone);
+    deliveryRepository.save(delivery);
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.delete("/delivery/{id}", delivery.getId())
+                .contentType("application/json"))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+}
---
+++ src/test/java/com/future8/droneapp/controlller/DroneTest.java
@@ -0,0 +1,88 @@
+package com.future8.droneapp.controlller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.future8.droneapp.model.Drone;
+import com.future8.droneapp.repository.DroneRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DroneTest {
+  @Autowired private MockMvc mockMvc;
+
+  @SpyBean private DroneRepository droneRepository;
+
+  @Autowired private ObjectMapper objectMapper;
+
+  @BeforeEach
+  public void initEach() {
+    droneRepository.deleteAll();
+  }
+
+  @Test
+  @DisplayName("1 - Drone can be created with sucess")
+  public void create() throws JsonProcessingException, Exception {
+    Drone drone = new Drone(15151, 46545);
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post("/drone")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(drone)))
+        .andExpect(MockMvcResultMatchers.status().isCreated());
+  }
+
+  @Test
+  @DisplayName("2 - List all drones")
+  public void retornaTodosOsDrones() throws Exception {
+    Drone drone = new Drone(15151, 46545);
+    Drone drone2 = new Drone(2020, 2020);
+
+    droneRepository.save(drone);
+    droneRepository.save(drone2);
+
+    mockMvc
+        .perform(MockMvcRequestBuilders.get("/drone").contentType("application/json"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(drone.getId()))
+        .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(drone2.getId()));
+  }
+
+  @Test
+  @DisplayName("3 - Drone can be edited with sucess")
+  public void atualizaStatusDrone() throws Exception {
+    Drone drone = new Drone(15151, 46545);
+    droneRepository.save(drone);
+
+    drone.setLatitude(2020);
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.put("/drone/{id}", drone.getId())
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(drone)))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  @Test
+  public void deletaDronePeloId() throws Exception {
+    Drone drone = new Drone(15151, 46545);
+    droneRepository.save(drone);
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.delete("/drone/{id}", drone.getId())
+                .contentType("application/json"))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+}
